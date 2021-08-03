4c27b5abc09430a44269dfa024c130dd20771338
ugo <kitisugochukwu@gmail.com>
2021-08-03 11:55:57 +0100

    The first stage of ideas

---
+++ main.cpp
@@ -0,0 +1,41 @@
+/*
+ * THE MOVIE LOVERS APP
+ * 
+ * PURPOSE OF THE APP
+ *      To keep track of the movies they have watched and how many times they have watched the movie 
+ *
+ * THE MAKING OF THE APP
+ * 
+ *  There will be 2 Classes
+ * 
+ *  class movie
+ *  friend class Movies; 
+ *  private: 
+ *  vector <string> movie_name
+ *  vector <string> movie_rating ***( PG - 13, 18+ )***
+ *  vector <int> watch_count 
+ * 
+ * public:
+ * 
+ * 
+ *  class Movies
+ *  private:
+ *  std::string movie_title;
+ *  std::string rating;
+ *  int watch_count;
+ * 
+ *  public:
+ *  add_movie( std::string movie_title, std::string rating, int watch_count);
+ *  increment_count( std::string movie_title);
+ *  display_list();
+ *  
+ * */
+
+
+
+#include <iostream>
+
+int main() {
+    std::cout<<"My Template"<<std::endl;
+    return 0;
+}
